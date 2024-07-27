ae635e9776a29eed6f2c7cbb19733721700488b0
ValeryM <muntyanw@gmail.com>
2024-07-27 18:24:50 +0300

    v9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
+        $thiclears->call([
             UsersTableSeeder::class,
             SkillsTableSeeder::class,
             VolunteersTableSeeder::class,
